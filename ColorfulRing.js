68a6a56b4bf24aae2c4b9350c7dba331be3b35c8
shhu0217 <shhu0217@uni.sydney.edu.au>
2025-06-10 20:36:28 +1000

    Iteration2

--- ColorfulRing.js
+++ ColorfulRing.js
@@ -1,27 +1,50 @@
 class ColorfulRing {
   constructor(x, y, size) {
-    this.xpos = x;
-    this.ypos = y;
-    this.size = size;
+    this.xpos = x; 
+    this.ypos = y; 
+    this.size = size; 
+
+    this.typeOrder = shuffle([1, 2, 3]);
+
+    this.color1 = color(random(255), random(255), random(255));
+    this.color2 = color(random(255), random(255), random(255));
+    this.color3 = color(random(255), random(255), random(255));
   }
 
   show() {
     push();
     translate(this.xpos * canvasScale, this.ypos * canvasScale);
 
-    noStroke();
-    fill(33, 85, 149);
-    circle(0, 0, this.size * canvasScale);
-
-    noStroke();
-    fill(239, 174, 63);
-    circle(0, 0, this.size * canvasScale * 0.66);
-
-    noStroke();
-
-    fill(200, 70, 97);
-    circle(0, 0, this.size * canvasScale * 0.33);
-
+    for (let i = 0; i < this.typeOrder.length; i++) {
+      let scaling = 1 - (i + 1) / this.typeOrder.length;
+      let size = this.size * canvasScale * scaling;
+      let type = this.typeOrder[i];
+      if (type === 1) {
+        this.drawType1Circle(size);
+      } else if (type === 2) {
+        this.drawType2Circle(size);
+      } else if (type === 3) {
+        this.drawType3Circle(size);
+      }
+    }
     pop();
+  }
+
+  drawType1Circle(s) {
+    noStroke();
+    fill(this.color1);
+    circle(0, 0, s);
+  }
+
+  drawType2Circle(s) {
+    noStroke();
+    fill(this.color2);
+    circle(0, 0, s);
+  }
+
+  drawType3Circle(s) {
+    noStroke();
+    fill(this.color3);
+    circle(0, 0, s);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+let colorfulRings = []; 
+
+let canvasSize = 1000; 
+let canvasScale = 1; 
+let ringNumbers = 10; 
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasSize, canvasSize, P2D);
+  windowResized();
+  generateRandomRings();
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  showAllRings();
+}
+
+function windowResized() {
+  let minWinSize = min(windowWidth, windowHeight);
+  resizeCanvas(minWinSize, minWinSize);
+  canvasScale = minWinSize / canvasSize;
+}
+
+function generateRandomRings() {
+  for (let i = 0; i < ringNumbers; i++) {
+    let x = random(canvasSize);
+    let y = random(canvasSize);
+    let size = random(canvasSize * 0.3, canvasSize * 0.5);
+
+    let ring = new ColorfulRing(x, y, size);
+    colorfulRings.push(ring);
+  }
+}
+
+function showAllRings() {
+  for (let ring of colorfulRings) {
+    ring.show();
+  }
 }
