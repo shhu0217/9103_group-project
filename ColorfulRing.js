580e767f8042ac7e5f7e571b2ff938ea0de81f31
shhu0217 <shhu0217@uni.sydney.edu.au>
2025-06-10 20:59:34 +1000

    Iteration3

--- ColorfulRing.js
+++ ColorfulRing.js
@@ -1,8 +1,8 @@
 class ColorfulRing {
   constructor(x, y, size) {
-    this.xpos = x; 
+    this.xpos = x;
     this.ypos = y; 
-    this.size = size; 
+    this.size = size;
 
     this.typeOrder = shuffle([1, 2, 3]);
 
@@ -15,25 +15,44 @@ class ColorfulRing {
     push();
     translate(this.xpos * canvasScale, this.ypos * canvasScale);
 
-    for (let i = 0; i < this.typeOrder.length; i++) {
-      let scaling = 1 - (i + 1) / this.typeOrder.length;
-      let size = this.size * canvasScale * scaling;
-      let type = this.typeOrder[i];
-      if (type === 1) {
-        this.drawType1Circle(size);
-      } else if (type === 2) {
-        this.drawType2Circle(size);
-      } else if (type === 3) {
-        this.drawType3Circle(size);
-      }
+    if (this.typeOrder[0] === 1) {
+      this.drawType1Circle(this.size * canvasScale);
+    } else {
+      this.drawType3Circle(this.size * canvasScale);
     }
+
     pop();
   }
 
   drawType1Circle(s) {
-    noStroke();
+
+    stroke(this.color2);
+    strokeWeight(lineWeight);
     fill(this.color1);
     circle(0, 0, s);
+
+    noStroke();
+    fill(this.color2);
+
+    let r = s / 2;
+    let sapcing = s * 0.04; 
+    let circleNum = r / sapcing; 
+    let spotNum = s * 0.2; 
+    let spotDt = TWO_PI / spotNum; 
+    let offset = 0; 
+    let offsetDt = s * 0.1;
+
+    for (let i = 0; i < circleNum - 1; i++) {
+      offset += offsetDt;
+      for (let j = 0; j < spotNum; j++) {
+        let angle = j * spotDt + offset;
+        let raduis = i * sapcing;
+
+        let spotX = raduis * cos(angle);
+        let spotY = raduis * sin(angle);
+        circle(spotX, spotY, s * 0.03);
+      }
+    }
   }
 
   drawType2Circle(s) {
@@ -43,8 +62,21 @@ class ColorfulRing {
   }
 
   drawType3Circle(s) {
-    noStroke();
-    fill(this.color3);
+
+    stroke(this.color3);
+    strokeWeight(lineWeight);
+    fill(this.color2);
     circle(0, 0, s);
+
+    let sapcing = s * 0.1; 
+    let circleNum = s / sapcing; 
+
+    for (let i = circleNum - 1; i >= 0; i--) {
+      let raduis = i * sapcing;
+      let c = i % 2 === 0 ? this.color3 : this.color2;
+      noStroke();
+      fill(c);
+      circle(0, 0, raduis);
+    }
   }
 }
