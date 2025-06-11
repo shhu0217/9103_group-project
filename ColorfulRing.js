715e2b1d4b1306ab3ce8ab39a4be001e5aec9996
ygao0949 <ygao0949@uni.sydney.edu.au>
2025-06-11 21:30:37 +0800

    Iteration 5

--- ColorfulRing.js
+++ ColorfulRing.js
@@ -4,7 +4,7 @@ class ColorfulRing {
     this.ypos = y; 
     this.size = size; 
 
-    // Randomize the order of circle types
+    //  Randomize the order of circle types
     this.typeOrder = shuffle([1, 2, 3]);
     
     this.colorPatterns = shuffle([
@@ -20,7 +20,7 @@ class ColorfulRing {
     ]);
   }
 
-  // Render the visual appearance of the ring
+  //  Render the visual appearance of the ring
   show() {
     push();
     translate(this.xpos * canvasScale, this.ypos * canvasScale);
@@ -30,11 +30,11 @@ class ColorfulRing {
       let size = this.size * canvasScale * scaling;
       let type = this.typeOrder[i];
       if (type === 1) {
-        this.drawType2Circle(size);
+        this.drawType1Circle(size);
       } else if (type === 2) {
         this.drawType2Circle(size);
       } else if (type === 3) {
-        this.drawType2Circle(size);
+        this.drawType3Circle(size);
       }
     }
     pop();
@@ -42,14 +42,42 @@ class ColorfulRing {
 
   //  Draw circle type 1
   drawType1Circle(s) {
-    noStroke();
+    
+    stroke(this.colorPatterns[1]);
+    strokeWeight(lineWeight);
     fill(this.colorPatterns[0]);
     circle(0, 0, s);
+
+    
+    noStroke();
+    fill(this.colorPatterns[1]);
+
+    let r = s / 2;
+    let sapcing = s * 0.04; 
+    if (sapcing <= 0) return;
+    let circleNum = ceil(r / sapcing); 
+    let spotNum = s * 0.2; 
+    let spotDt = TWO_PI / spotNum; 
+    let offset = 0; 
+    let offsetDt = s * 0.1;
+
+    
+    for (let i = 0; i < circleNum - 1; i++) {
+      offset += offsetDt;
+      for (let j = 0; j < spotNum; j++) {
+        let angle = j * spotDt + offset;
+        let raduis = i * sapcing;
+
+        let spotX = raduis * cos(angle);
+        let spotY = raduis * sin(angle);
+        circle(spotX, spotY, s * 0.03);
+      }
+    }
   }
 
   // Draw circle type 2: line segment pattern
   drawType2Circle(s) {
-    //  Draw the background color
+    // Draw the background color
     stroke(this.colorPatterns[2]);
     strokeWeight(lineWeight);
     fill(this.colorPatterns[1]);
@@ -85,10 +113,25 @@ class ColorfulRing {
     endShape(CLOSE);
   }
 
-  // Draw circle type 3
+  // Draw circle type 3: concentric ring pattern
   drawType3Circle(s) {
-    noStroke();
+    
+    stroke(this.colorPatterns[3]);
+    strokeWeight(lineWeight);
     fill(this.colorPatterns[2]);
     circle(0, 0, s);
+
+    
+    let sapcing = s * 0.1; 
+    if (sapcing <= 0) return;
+    let circleNum = round(s / sapcing); 
+
+    for (let i = circleNum - 1; i >= 0; i--) {
+      let raduis = i * sapcing;
+      let c = i % 2 === 0 ? this.colorPatterns[3] : this.colorPatterns[2];
+      noStroke();
+      fill(c);
+      circle(0, 0, raduis);
+    }
   }
 }
--- main.js
+++ main.js
@@ -2,7 +2,10 @@ let colorfulRings = [];
 
 let canvasSize = 1000; 
 let canvasScale = 1; 
-let ringNumbers = 10; 
+let ringNumbers = 100; 
+let minRadius = canvasSize * 0.2; 
+let maxRadius = canvasSize * 0.8; 
+let maxAttempts = 100000; 
 let lineWeight = 4; 
 
 function setup() {
@@ -17,30 +20,45 @@ function draw() {
   showAllRings();
 }
 
-
+//  Resize the canvas when the window is resized
 function windowResized() {
   let minWinSize = min(windowWidth, windowHeight);
   resizeCanvas(minWinSize, minWinSize);
   canvasScale = minWinSize / canvasSize;
 }
 
-//Randomly generate circle objects with varying positions and sizes
+// Randomly generate circle objects with varying positions and sizes
 function generateRandomRings() {
-  colorfulRings = [];
-  for (let i = 0; i < ringNumbers; i++) {
+  let attempts = 0;
+ 
+  while (colorfulRings.length < ringNumbers && attempts < maxAttempts) {
+   
     let x = random(canvasSize);
     let y = random(canvasSize);
-    let size = random(canvasSize * 0.3, canvasSize * 0.5);
+    let size = random(minRadius, maxRadius);
 
-    let ring = new ColorfulRing(x, y, size);
-    colorfulRings.push(ring);
+    let overlapping = false;
+    // Check if the new circle overlaps with any existing ones
+    for (let other of colorfulRings) {
+      let d = dist(x, y, other.xpos, other.ypos);
+      if (d < size * 0.25 + other.size * 0.25) {
+        overlapping = true;
+        break;
+      }
+    }
+
+    // If not overlapping, add the new ring to the array
+    if (!overlapping) {
+      colorfulRings.push(new ColorfulRing(x, y, size));
+    } else {
+      attempts++;
+    }
   }
 }
 
-
+//  Display all rings on the canvas
 function showAllRings() {
   for (let ring of colorfulRings) {
     ring.show();
   }
 }
-
